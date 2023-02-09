fd650c16c992414f9180c0e8c914544f9793a4e4
David Stumpe <dstumpe264@gmail.com>
2023-02-08 19:48:25 -0500

    created userRoutes.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+
+// import models
+
+// Get all users
+
+// Get a single user
+
+// Create a user
+
+// Update a user
+
+// Delete a user
+
+module.exports = router;

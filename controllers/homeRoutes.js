5d5868781c83129e28b6ddcf2646e21812b9f0fd
ALiteralWaffle <43278458+ALiteralWaffle@users.noreply.github.com>
2023-02-13 19:18:29 -0500

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,5 +19,14 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 
 module.exports = router;
